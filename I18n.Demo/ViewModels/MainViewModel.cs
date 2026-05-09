9f2f830c10f5e15b61e9cfba15c541386102c764
XIOA <102797328+xioa-cn@users.noreply.github.com>
2026-05-09 23:10:27 +0800

    Enhance MainViewModel with ComboBox and localization
    Added ComboBox support and localization features in MainViewModel.

--- I18n.Demo/ViewModels/MainViewModel.cs
+++ I18n.Demo/ViewModels/MainViewModel.cs
@@ -1,10 +1,13 @@
-﻿using I18n.Core.Example;
+using I18n.Core.Example;
 using I18n.Core.Models;
 using I18n.Core.Wpf.Extensions;
 using I18n.Demo.Command;
+using I18n.Demo.Models;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Markup;
 
 namespace I18n.Demo.ViewModels
 {
@@ -18,7 +21,19 @@ namespace I18n.Demo.ViewModels
 
         public ObservableCollection<LangSource> LangSources { get; set; }
 
+        private ComboBoxSelectValueEnum _TestValue;
 
+        public ComboBoxSelectValueEnum TestValue
+        {
+            get => _TestValue;
+            set
+            {
+                _TestValue = value;
+                OnPropertyChanged(nameof(TestValue));
+            }
+        }
+
+        public List<ComboBoxItem> ComboBoxItems { get; } = new();
         public MainViewModel()
         {
             LangSources =
@@ -27,6 +42,24 @@ namespace I18n.Demo.ViewModels
                 new LangSource() { Name = "English", SourceKey = "en" },
             ];
             ChangeLangCommand = new RelayCommand<LangSource>(ChangeLang);
+
+
+            var itemList = new List<(string Key, ComboBoxSelectValueEnum Value)>
+                        {
+                            ("ComboBox.Value1", ComboBoxSelectValueEnum.None),
+                            ("ComboBox.Value2", ComboBoxSelectValueEnum.Test1)
+                        };
+
+            foreach (var (key, enumValue) in itemList)
+            {
+                var item = new ComboBoxItem();
+                var ext = new LocalizeExtension(key);
+
+                var serviceProvider = new ServiceProvider(item, ComboBoxItem.ContentProperty);
+                item.Content = ext.ProvideValue(serviceProvider);
+                item.Tag = enumValue;
+                ComboBoxItems.Add(item);
+            }
         }
 
         private void ChangeLang(LangSource source)
@@ -36,5 +69,29 @@ namespace I18n.Demo.ViewModels
 
         public ICommand ChangeLangCommand { get; set; }
 
+    }
+
+
+    public class ServiceProvider : IServiceProvider, IProvideValueTarget
+    {
+        private readonly object _targetObject;
+        private readonly object _targetProperty;
+
+        public ServiceProvider(object targetObject, object targetProperty)
+        {
+            _targetObject = targetObject;
+            _targetProperty = targetProperty;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            // 你的 LocalizeExtension 只需要这个服务，所以直接返回自己
+            if (serviceType == typeof(IProvideValueTarget))
+                return this;
+            return null;
+        }
+
+        public object TargetObject => _targetObject;
+        public object TargetProperty => _targetProperty;
     }
 }
