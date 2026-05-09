f64f1228c82df8c36fb93f1f8e10edc719a40c27
XIOA <102797328+xioa-cn@users.noreply.github.com>
2026-05-09 23:12:28 +0800

    Add ComboBoxSelectValueEnum for selection values

---
+++ I18n.Demo/Models/ComboBoxSelectValueEnum.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I18n.Demo.Models
+{
+    public enum ComboBoxSelectValueEnum
+    {
+
+        None = 0,
+        Test1,
+        Test2, Test3, Test4,
+    }
+}
